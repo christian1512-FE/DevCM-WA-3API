5aef76ea8627f80f77e43bbd6911fdde0e7d9594
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2022-12-07 13:58:40 -0600

    html/css

---
+++ Assets/js/script.js
@@ -0,0 +1,32 @@
+
+var searchBTNEl = document.getElementById('searchbtn');
+
+var searchInputEl = document.getElementById('search-input');
+var citylistulEl = document.getElementById('city-historyLi');
+// var cityGroupEl = document.querySelectorAll('input-group');
+var inputformEL = document.getElementsByClassName(".input-city")
+var pastSearches = [];
+
+
+
+// // API KEY
+// var APIkey = "5789e00f7f8bf8619815be00e90a99c4"; //NOTE: "string"
+
+// var city = 'austin';
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+// function getAPI() {
+
+//     fetch('queryURL')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+// }
+// searchBTNEl.addEventListener('click', getAPI);
+// console.log()
+
+
