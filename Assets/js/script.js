400d38b777f7de9c8f6c56db2a2484b75d395ecf
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2022-12-08 11:55:21 -0600

    working-on-css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,20 +1,49 @@
 
+// SEARCH ELEMENTS
 var searchBTNEl = document.getElementById('searchbtn');
-
-var searchInputEl = document.getElementById('search-input');
+var searchInputEl = document.getElementById('searchinputVal');
+var inputformEL = document.getElementsByClassName("city")
 var citylistulEl = document.getElementById('city-historyLi');
 // var cityGroupEl = document.querySelectorAll('input-group');
-var inputformEL = document.getElementsByClassName(".input-city")
+
 var pastSearches = [];
 
+// WEATHER ELEMENTS
+
+//get Date(); 
+var currentDateEl = document.getElementById("date")
+//get Day();
+var dayOneEl = document.getElementById("date1")
+//TEMP
+var tempeEl = document.getElementById("temperature");
+
+//API KEY
+
+var APIkey = "5789e00f7f8bf8619815be00e90a99c4"; //NOTE: "string"
+
+var city = "";
+
+// var state;
+// var zipCode;
 
 
-// // API KEY
-// var APIkey = "5789e00f7f8bf8619815be00e90a99c4"; //NOTE: "string"
+ 
+// var getCity = function(city) {
 
-// var city = 'austin';
+// var cityAPI = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid={5789e00f7f8bf8619815be00e90a99c4}";
 
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+//     fetch('cityAPI')
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+// }
+// searchBTNEl.addEventListener('click', getCity);
+
+// });
+
 
 // function getAPI() {
 
@@ -30,3 +59,38 @@ var pastSearches = [];
 // console.log()
 
 
+
+
+
+
+
+
+
+
+// 5 DAY WEATHER FORECAST 
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// REQUIRES: lat, lon
+// appid: API key 
+
+// https://openweathermap.org/forecast5#limit (LINK FOR REFERENCE)
+
+//TEMPERATURE: cnt For temperature in Fahrenheit use units=imperial
+
+//EXAMPLE CALL BELOW:
+// https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API KEY} &units=imperial
+
+
+//MULTILINGUAL SUPPORT
+//You can use the lang parameter to get the output in your language. 
+// Translation is applied to the city name and description fields.
+// API CALL EX: 
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&lang={lang}
+// lang=en
+// CALL BACK FUNCTION
+// api.openweathermap.org/data/2.5/forecast?q=London,uk&callback=test&appid={API key}
+
+
+
+// lat & lon API CALL need it to display weather image
+// 5 day api call 
